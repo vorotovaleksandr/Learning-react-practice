5e4290f41e4de69b9fb3b78db19d04df3fb0064c
vorotovaleksandr <vorotovaleksandr.exceedteam@gmail.com>
2019-04-22 20:28:10 +0300

    22/04/19

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import classes from './ActiveQuiz.css'
+import AnswersList from './AnswersList/AnswersList'
+
+const ActiveQuiz = props =>(
+    <div className={classes.ActiveQuiz}>
+      <p className={classes.Question}>
+        <span>
+          <strong>2.</strong>&nbsp;
+           Как делa?
+        </span>
+
+        <small>4 из 12</small>
+      </p>
+      <AnswersList
+        answers={props.answers}
+      />
+
+    </div>
+
+)
+
+export default ActiveQuiz
